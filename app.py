0398232f34bff9cff8ecdd5438adc2d4d79d69c4
Abel <abel.perez@gmail.com>
2026-02-28 17:38:06 +0100

    Keyboard device

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from time import sleep
 from unittest import case
 
+from core.KeyboardDevice import KeyboardDevice
 from core.KeyboardEmulator import KeyboardEmulator
 from core.Keyer import Keyer
 from core.SoundProcessor import SoundProcessor
@@ -11,7 +12,9 @@ from os import system
 
 class App:
     def __init__(self):
-        self._device = None
+        self._usb_device = None
+        self._keyboard_device = None
+        self._is_keyboard = False
         #####
         self._keyboard_emulator = None
         self._keyer = None
@@ -38,13 +41,13 @@ class App:
     def _start_keyboard_emulator(self):
         if self._keyboard_emulator is  None:
             self._keyboard_emulator = KeyboardEmulator()
-            self._device.attach_observer(self._keyboard_emulator)
+            self._usb_device.attach_observer(self._keyboard_emulator)
         else:
             print("Keyboard keyer is already running.")
 
     def _stop_keyboard_emulator(self):
         if self._keyboard_emulator is not None:
-            self._device.detach_observer(self._keyboard_emulator)
+            self._usb_device.detach_observer(self._keyboard_emulator)
             self._keyboard_emulator = None
         else:
             print("Keyboard keyer is not running, skipping stop.")
@@ -53,42 +56,64 @@ class App:
         if self._keyer is None:
             self._keyer = Keyer(wpm=20)
             self._keyer.start()
-            self._device.attach_observer(self._keyer)
+            if self._usb_device is not None:
+                self._usb_device.attach_observer(self._keyer)
+            elif self._keyboard_device is not None:
+                self._keyboard_device.attach_observer(self._keyer)
+            self._start_sound_processor()
         else:
             print("Keyer is already running.")
 
     def _stop_keyer(self):
         if self._keyer is not None:
-            self._device.detach_observer(self._keyer)
+            if self._usb_device is not None:
+                self._usb_device.detach_observer(self._keyer)
+            elif self._keyboard_device is not None:
+                self._keyboard_device.detach_observer(self._keyer)
             self._keyer.stop()
             self._keyer = None
         else:
             print("Keyer is not running, skipping stop.")
 
     def _start_usb_device(self):
-        if self._device is None:
-            self._device = UsbDevice(0x413d,0x2107)
-            self._device.start()
+        if self._usb_device is None:
+            self._usb_device = UsbDevice(0x413d,0x2107)
+            self._usb_device.start()
         else:
             print("USB device is already running.")
 
-    def _stop_device(self):
-        if self._device is not None:
-            self._device.stop()
-            self._device = None
+    def _start_keyboard_device(self):
+        if self._keyboard_device is None:
+            self._keyboard_device = KeyboardDevice()
+            self._keyboard_device.start()
         else:
-            print("Device is not running, skipping stop.")
+            print("Keyboard device is already running.")
+
+    def _stop_keyboard_device(self):
+        if self._keyboard_device is not None:
+            self._keyboard_device.stop()
+            self._keyboard_device = None
+        else:
+            print("Keyboard device is not running, skipping stop.")
+
+    def _stop_usb_device(self):
+        if self._usb_device is not None:
+            self._usb_device.stop()
+            self._usb_device = None
+        else:
+            print("USB Device is not running, skipping stop.")
 
     def _stop(self):
         self._stop_sound_processor()
         self._stop_keyer()
         self._stop_keyboard_emulator()
-        self._stop_device()
+        self._stop_usb_device()
+        self._stop_keyboard_device()
 
 
     def _menu(self):
-        print("1. Input USB device", "OFF" if self._device is None else "ON")
-        print("2. Input Keyboard device: " )
+        print("1. Input USB device", "OFF" if self._usb_device is None else "ON")
+        print("2. Input Keyboard device: ", "OFF" if self._keyboard_device is None else "ON" )
         print("3. Keyboard emulator: ", "OFF" if self._keyboard_emulator is None else "ON" )
         print("4. Sound processor: ", "OFF" if self._sound_processor is None else "ON" )
         print("5. Keyer: ", "OFF" if self._keyer is None else "ON")
@@ -100,23 +125,30 @@ class App:
 
         match val:
             case 1:
-                if self._device is None:
+                self._stop()
+                if self._usb_device is None:
                     self._start_usb_device()
                 else:
-                    self._stop_device()
+                    self._stop_usb_device()
             case 2:
-                print("Not implemented yet.")
+                self._stop()
+                if self._keyboard_device is None:
+                    self._start_keyboard_device()
+                else:
+                    self._stop_keyboard_device()
 
             case 3:
                 if self._keyboard_emulator is None:
                     self._start_keyboard_emulator()
                 else:
                     self._stop_keyboard_emulator()
+
             case 4:
                 if self._sound_processor is None:
                     self._start_sound_processor()
                 else:
                     self._stop_sound_processor()
+
             case 5:
                 if self._keyer is None:
                     self._start_keyer()
--- core/Keyer.py
+++ core/Keyer.py
@@ -129,7 +129,7 @@ class Keyer(DeviceObserver):
     """
     def _send_dit(self) :
         total = self._dit_time + self._space_time
-        print("SEND DIT {}s ".format(total))
+        #print("SEND DIT {}s ".format(total))
 
         if len(self._observers) > 0:
             for observer in self._observers:
@@ -146,7 +146,7 @@ class Keyer(DeviceObserver):
     """
     def _send_dah(self):
         total = self._dah_time + self._space_time
-        print("SEND DAH {}s".format(total))
+        #print("SEND DAH {}s".format(total))
 
         for observer in self._observers:
             self._thread_pool.submit(observer.play_dah, self._dah_time, self._space_time)
