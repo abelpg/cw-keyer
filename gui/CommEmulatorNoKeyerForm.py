16f5e65ba4a927094307fca7a107d1413376cd73
Abel <abel.perez@gmail.com>
2026-03-06 19:28:02 +0100

    Abstract serial form

--- gui/CommEmulatorNoKeyerForm.py
+++ gui/CommEmulatorNoKeyerForm.py
@@ -5,63 +5,23 @@ from PySide6 import QtWidgets
 from core.config import Configuration
 from core.emulator import CommEmulator
 from core.emulator.CommSerial import CommSerial
+from gui.SerialForm import SerialForm
 
+class CommEmulatorNoKeyerForm(SerialForm):
 
-class CommEmulatorNoKeyerForm:
     def __init__(self, parent: QtWidgets.QBoxLayout,
                  callback_attach_device_observer=None,
                  callback_detach_device_observer=None):
-        self._logger = logging.getLogger(__name__)
-
-        self.parent = parent
+        super().__init__(parent, __name__, button_text= "Comm emulator",callback_click=self._click_comm_emulator )
         self._callback_attach_device_observer = callback_attach_device_observer
         self._callback_detach_device_observer = callback_detach_device_observer
-
-        self._config = Configuration()
-
-        self._comm_emulator = None
-
-        layout = QtWidgets.QHBoxLayout()
-        widget = QtWidgets.QWidget()
-        widget.setMaximumHeight(45)
-
-        widget.setLayout(layout)
-
-        self._button_comm_emulator = QtWidgets.QPushButton("Comm emulator")
-        self._button_comm_emulator.clicked.connect(self._click_comm_emulator)
-
-        self._comm_emulator_port = QtWidgets.QComboBox()
-        self._set_ports()
-
-        layout.addWidget(self._button_comm_emulator)
-        layout.addWidget(self._comm_emulator_port)
-
-        parent.addWidget(widget)
-
-    def _set_ports(self):
-        if self._comm_emulator_port is not None:
-            config_port = self._config.get_config(__name__, "comm_emulator_port")
-            index = 0
-            found = False
-            for port in CommSerial.list_ports():
-                self._comm_emulator_port.addItem(port, port)
-                if port == config_port and not found:
-                    found = True
-                elif not found:
-                    index += 1
-            self._logger.debug(f"Found {index} {found} ports.")
-            if found:
-                self._comm_emulator_port.setCurrentIndex(index)
+        self._logger = logging.getLogger(__name__)
 
 
     def _start_comm_emulator(self):
         # Protect concurrent loop
         if self._comm_emulator is  None:
-            port = self._comm_emulator_port.currentData()
-
-            self._config.put_config(__name__, "comm_emulator_port", port)
-
-            self._comm_emulator = CommEmulator(port=port)
+            self._comm_emulator = CommEmulator(port=super()._get_port())
             self._callback_attach_device_observer(self._comm_emulator)
             self._comm_emulator.start()
 
---
+++ gui/SerialForm.py
@@ -0,0 +1,56 @@
+import logging
+
+from PySide6 import QtWidgets
+from core.config import Configuration
+from core.emulator import CommEmulator
+from core.emulator.CommSerial import CommSerial
+
+class SerialForm:
+
+    def __init__(self, parent: QtWidgets.QBoxLayout, class_name, button_text : str, callback_click=None):
+        self._logger = logging.getLogger(__name__)
+
+        self.parent = parent
+        self._config = Configuration()
+
+        self._class_name = class_name
+
+        self._comm_emulator = None
+
+        layout = QtWidgets.QHBoxLayout()
+        widget = QtWidgets.QWidget()
+        widget.setMaximumHeight(45)
+
+        widget.setLayout(layout)
+
+        self._button_comm_emulator = QtWidgets.QPushButton(button_text)
+        self._button_comm_emulator.clicked.connect(callback_click)
+
+        self._comm_emulator_port = QtWidgets.QComboBox()
+        self._set_ports()
+
+        layout.addWidget(self._button_comm_emulator)
+        layout.addWidget(self._comm_emulator_port)
+
+        parent.addWidget(widget)
+
+    def _set_ports(self):
+        if self._comm_emulator_port is not None:
+            config_port = self._config.get_config(self._class_name, "comm_emulator_port")
+            index = 0
+            found = False
+            for port in CommSerial.list_ports():
+                self._comm_emulator_port.addItem(port, port)
+                if port == config_port and not found:
+                    found = True
+                elif not found:
+                    index += 1
+            self._logger.debug(f"Found {index} {found} ports.")
+            if found:
+                self._comm_emulator_port.setCurrentIndex(index)
+
+    def _get_port(self):
+        port = self._comm_emulator_port.currentData()
+        self._config.put_config(self._class_name, "comm_emulator_port", port)
+        return port
+
